4fd4e320642d0fcf1d31eee455b11b8f2923c558
sofiaroman1969 <maria.roman@tecdesoftware.edu.mx>
2025-11-12 12:08:37 -0600

    Ejercicio 11 resuelto

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -452,20 +452,33 @@ function rotar90Grados(matriz) {
  */
 function mezclarImagenes(matriz1, matriz2, factor) {
   // TODO: Implementar mezcla de imágenes
-  
   // 1. Verificar que tengan las mismas dimensiones
-  // const dims1 = obtenerDimensiones(matriz1);
-  // const dims2 = obtenerDimensiones(matriz2);
-  // if (dims1.filas !== dims2.filas || dims1.columnas !== dims2.columnas) {
-  //   throw new Error('Las imágenes deben tener el mismo tamaño');
-  // }
-  
-  // 2. Para cada pixel:
-  // r = r1 * (1 - factor) + r2 * factor
-  // g = g1 * (1 - factor) + g2 * factor
-  // b = b1 * (1 - factor) + b2 * factor
-  
-  return []; // REEMPLAZAR
+  const dims1 = obtenerDimensiones(matriz1);
+  const dims2 = obtenerDimensiones(matriz2);
+  if (dims1.filas !== dims2.filas || dims1.columnas !== dims2.columnas) {
+    throw new Error('Las imágenes deben tener el mismo tamaño');
+  }
+
+  const resultado = crearMatrizVacia(dims1.columnas, dims1.filas);
+
+  for (let i = 0; i < dims1.filas; i++) {
+    for (let j = 0; j < dims1.columnas; j++) {
+      const p1 = matriz1[i][j];
+      const p2 = matriz2[i][j];
+      const r = Math.round(p1.r * (1 - factor) + p2.r * factor);
+      const g = Math.round(p1.g * (1 - factor) + p2.g * factor);
+      const b = Math.round(p1.b * (1 - factor) + p2.b * factor);
+      const a = Math.round(p1.a * (1 - factor) + p2.a * factor);
+      resultado[i][j] = {
+        r: limitarValorColor(r),
+        g: limitarValorColor(g),
+        b: limitarValorColor(b),
+        a: limitarValorColor(a)
+      };
+    }
+  }
+
+  return resultado;
 }
 
 /**
